14e08686b2eedf028b4ff688e1495e825eb4b639
foocoder <slow295185031@gmail.com>
2016-08-12 21:36:17 +0800

    add non-complete mixed color

---
+++ Netease/MixedColor/MixedColor.cpp
@@ -0,0 +1,17 @@
+// ---- Program Info Start----
+//FileName:     MixedColor.cpp
+//
+//Author:       Fuchen Duan
+//
+//Email:        slow295185031@gmail.com
+//
+//CreatedAt:    2016-08-08 10:04:27
+// ---- Program Info End  ----
+
+#include <iostream>
+
+using namespace std;
+
+int solutions( int n, int * colors ){
+
+}
